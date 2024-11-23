6c93a186406877d1b78014e81375249ef8e6d3a3
Yoshio Nomura <nomurin.alex103@gmail.com>
2024-11-23 23:33:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import os
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "https://tech0-gen-8-step3-testapp-node1-15.azurewebsites.net"}})  # CORS設定を更新
@@ -31,5 +30,4 @@ def echo():
     return jsonify({"message": f"echo: {message}"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', 
-    port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True)
