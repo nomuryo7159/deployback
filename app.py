701b6c88c792e09ff8fc0ffd9c303642d5621ae8
Yoshio Nomura <nomurin.alex103@gmail.com>
2024-11-23 16:20:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -31,5 +31,4 @@ def echo():
     return jsonify({"message": f"echo: {message}"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', 
-    port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True)
